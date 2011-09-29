55c690a3f49e26547adbc34415cb2e918758b795
Sam Epstein <same008@gmail.com>
2011-09-29 18:02:01 -0400

    Some small changes to server.js

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 var express = require('express'),
-	fs = require('fs'),
-	path = require('path');
+	 fs = require('fs'),
+	 path = require('path'),
+	 rorrimFolder = path.join(process.env.HOME, '.rorrim/'),
+	 hostsDir = path.join(rorrimFolder, 'hosts'),
+	 app;
 
-express.createServer(function(req, res, next){
-	if (req.method === 'GET') {
-		fs.readdir(path.join(__dirname, 'hosts'), function(err, dirs){
-			var host = req.headers.host, name;
-			if (err) {
-				next(err);
-			} else if (dirs.indexOf(req.headers.host) != -1) {
-				express.static(path.join(__dirname, 'hosts', req.headers.host))(req, res, next);
-			} else if (req.url === '/') {
-				res.send('rorrim', { 'Content-Type': 'text/plain; charset=utf-8' });
-			} else {
-				next();
-			}
-		});
-	} else {
-		next();
+app = express.createServer(
+	function(req, res, next){
+		if (req.method === 'GET') {
+			fs.readdir(hostsDir, function(err, dirs){
+				var host = req.headers.host, name;
+				if (err) {
+					next(err);
+				} else if (dirs.indexOf(req.headers.host) != -1) {
+					express.static(path.join(hostsDir, req.headers.host))(req, res, next);
+				} else {
+					next();
+				}
+			});
+		} else {
+			next();
+		}
 	}
-}).listen(80);
+);
+app.get('/', function(req, res, next){
+	res.send('rorrim', { 'Content-Type': 'text/plain; charset=utf-8' });
+});
+module.exports.app = app;
